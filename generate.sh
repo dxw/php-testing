dda3867e08ee8426a888bbcbff32002c50937c32
Tom Adams <tom@dxw.com>
2015-12-01 16:35:59 -0500

    Add mbstring

--- generate.sh
+++ generate.sh
@@ -8,6 +8,7 @@ for I in 5.5 5.6 7.0; do
   echo '# Install composer and dependencies' >> $I/Dockerfile
   echo 'RUN apt-get update && apt-get install -y --no-install-recommends git zlib1g-dev ca-certificates' >> $I/Dockerfile
   echo 'RUN docker-php-ext-install zip' >> $I/Dockerfile
+  echo 'RUN docker-php-ext-install mbstring' >> $I/Dockerfile
   echo 'RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer' >> $I/Dockerfile
   echo '' >> $I/Dockerfile
   echo '# Install php-cs-fixer' >> $I/Dockerfile
