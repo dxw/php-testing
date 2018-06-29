de695d4befcfa4fad12f2fbdd61761b4adff6a98
Tom Adams <tom@dxw.com>
2018-06-29 08:45:08 -0400

    Add extensions: pcntl, sysvsem, sockets
    These are required by certain libraries for running PHP asynchronously.

--- generate.sh
+++ generate.sh
@@ -12,6 +12,9 @@ gen() {
   echo 'RUN apt-get update && apt-get install -y --no-install-recommends git zlib1g-dev ca-certificates' >> ${NAME}/Dockerfile
   echo 'RUN docker-php-ext-install zip' >> ${NAME}/Dockerfile
   echo 'RUN docker-php-ext-install mbstring' >> ${NAME}/Dockerfile
+  echo 'RUN docker-php-ext-install pcntl' >> ${NAME}/Dockerfile
+  echo 'RUN docker-php-ext-install sysvsem' >> ${NAME}/Dockerfile
+  echo 'RUN docker-php-ext-install sockets' >> ${NAME}/Dockerfile
   echo 'RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer' >> ${NAME}/Dockerfile
   echo '' >> ${NAME}/Dockerfile
 }
